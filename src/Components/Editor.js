12c1a32c84a1dbf4ca47430f9133062fb9d17151
faiza203 <fiazfaiza981@gmail.com>
2021-05-12 12:30:01 +0500

    Successfly marked

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,10 +1,16 @@
 import ToolBar from "./ToolBar";
+import placeholder, { changePlaceHolder } from "./PlaceHolder";
+
 function Editor() {
   return (
     <div className="editor">
       <ToolBar name="Editor" id="editTool" />
-      <textarea id="editorText" type="">
-      </textarea>
+      <textarea
+        id="editorText"
+        type="text"
+        defaultValue={placeholder}
+        onChange={changePlaceHolder}
+      ></textarea>
     </div>
   );
 }
--- src/Components/Previewer.js
+++ src/Components/Previewer.js
@@ -1,4 +1,6 @@
 import ToolBar from "./ToolBar";
+import placeholder from "./PlaceHolder";
+import marked from "marked";
 
 function Previewer() {
   return (
@@ -9,4 +11,10 @@ function Previewer() {
   );
 }
 
+setTimeout(() => {
+  const preText = document.querySelector(".previewerText");
+  preText.innerHTML = marked(placeholder);
+  document.querySelector(".loader").style.visibility = "hidden";
+  document.getElementById("container").style.visibility = "visible";
+}, 100);
 export default Previewer;
